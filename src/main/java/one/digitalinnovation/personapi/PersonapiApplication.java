eea0912f1503e1f4f5c665ddd78741ecdec4fe64
sp1249 <sp1249@uol.com.br>
2021-09-18 18:24:29 -0400

    Set Java version = 11 in file system.properties

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -1,4 +1,4 @@
-package com.one.digitalinnovation.personapi;
+package one.digitalinnovation.personapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
