ecfb309307b9634681bd94da2a6887306668b811
sp1249 <sp1249@uol.com.br>
2021-09-19 14:12:05 -0400

    Edit PersonService, PersonController Change file pom.xml to correct errors in PhoneDTO and PersonDTO CreatePersonMapper

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -8,6 +8,9 @@ import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 //Classe para colocar todas as regras de negócio da aplicação
 
@@ -30,5 +33,14 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+
     }
 }
